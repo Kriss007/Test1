54ffa0f42d53d8c6053c10e6f371e15f6c868c17
kriss007 <kriss4050@gmail.com>
2019-09-23 14:21:47 +0530

    adding new line from git/Intellij

--- FirstApp.java
+++ FirstApp.java
@@ -1,2 +1,3 @@
 This is a Simple Text file in which we made changes from Intellij.
 After Adding file from Intellij now adding comment from Github.
+Add some text from Intellj and then pushing same.3
