77d6d95446cfa1af0b8434e94068b6803ce8a96e
Kriss007 <49573104+Kriss007@users.noreply.github.com>
2019-09-23 13:55:15 +0530

    Update FirstApp.java

--- FirstApp.java
+++ FirstApp.java
@@ -1,3 +1,3 @@
 This is a Simple Text file in which we made changes from Intellij.
 After Adding file from Intellij now adding comment from Github.
->>>>>>> ba78d6123d03a20248cba718e841c97d98a5948f
+Adding one line from Github. 
