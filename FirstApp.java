0a769ae8e900dc2c5d6052012981990ac5f133ba
Kriss007 <49573104+Kriss007@users.noreply.github.com>
2019-09-23 14:24:25 +0530

    Doing in Github panel.

--- FirstApp.java
+++ FirstApp.java
@@ -1,3 +1,4 @@
 This is a Simple Text file in which we made changes from Intellij.
 After Adding file from Intellij now adding comment from Github.
 Add some text from Intellj and then pushing same.3
+Added a line from Github panel which will be not available in Local.
